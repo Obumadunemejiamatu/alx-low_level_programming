28f8f6eacaaa3709adef0b2e98b6a1c864099d5f
Obumadunemejiamatu <obummadunaeme@gmail.com>
2022-12-10 01:42:12 -0800

    Print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,13 +4,13 @@
  * main - main block
  * Description: Get a random it with 5
  * Return: 0
- * */
+ */
 int main(void)
 {
 
 	char c = 'a';
 
-	while(c  <= 'z')
+	while (c  <= 'z')
 	{
 
 		putchar(c);
