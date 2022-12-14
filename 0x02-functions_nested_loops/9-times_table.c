9d6fcad0b1c390e0510589a71666763e5d97180f
Obumadunemejiamatu <obummadunaeme@gmail.com>
2022-12-14 07:13:19 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,7 @@ void times_table(void)
 {
 	int a, b, c;
 
-	for (a = 0; <= 9; a++)
+	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
@@ -22,7 +22,7 @@ void times_table(void)
 				if (b != 0)
 				{
 					_putchar(' ');
-					_putchar(c % 10) + '0);
+					_putchar(c % 10) + '0');
 				}
 				if (b < 9)
 				{
@@ -34,7 +34,7 @@ void times_table(void)
 			{
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
-				if (b = 9)
+				if [b = 9]
 				{
 					_putchar(',');
 					_putchar(' ');
