5056cd865cd5d6c983e6beb7cd7c900ee80a6996
Obumadunemejiamatu <obummadunaeme@gmail.com>
2022-12-16 06:30:46 -0800

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * more_numbers - prints numbers to 14
+ *more_numbers - prints numbers to 14
  *
- * Return: returns nothing
+ *Return: returns nothing
  */
 void more_numbers(void)
 {
@@ -10,7 +10,7 @@ int number1, number2;
 
 for (number1 = 0; number1 < 10; number1++)
 {
-for (number2 = 0; number2 <= 14; number++)
+for (number2 = 0; number2 <= 14; number2++)
 
 {
 if (number2 > 9)
@@ -20,6 +20,7 @@ _putchar((number2 / 10) + '0');
 _putchar((number2 % 10) + '0');
 }
 _putchar(10);
+}
 }
 
 
