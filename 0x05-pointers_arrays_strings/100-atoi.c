05c4abcfe0934e9ce819b94e58c9295c8fe84e25
Obumadunemejiamatu <obummadunaeme@gmail.com>
2022-12-19 16:06:44 -0800

    100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,7 +15,7 @@ int _atoi(char *s)
 	temp = s;
 	num = 0;
 	sign = 1;
-	while (*temp != '\0' &&  (*temp < '0' || *temp > '9'))
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
 	{
 		if (*temp == '-')
 			sign *= -1;
@@ -24,7 +24,7 @@ int _atoi(char *s)
 	if (*temp != '\0')
 	{
 		do {
-			num = num * 10 + (*temp - .0);
+			num = num * 10 + (*temp - '0');
 			temp++;
 		} while (*temp >= '0' && *temp <= '9');
 	}
