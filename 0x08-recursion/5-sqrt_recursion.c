835c890c2692ef8a1d4c44ffdee787d9c44b801e
Obumadunemejiamatu <obummadunaeme@gmail.com>
2023-01-04 13:27:13 -0800

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,7 +5,8 @@
  * @n: integer
  * Return: evaluate sqrt
  */
-int _sqrt_recursion(int n)
+
+int _evaluate(int i, int n)
 {
 	/*Evaluate function*/
 	if (n == 0 || n == 1)
@@ -14,7 +15,7 @@ int _sqrt_recursion(int n)
 	else if (i * i < n)
 		return (_evaluate(i + 1, n));
 
-	else if (i * i == n) /*condition base*/
+	else if (i * i == n) /*Condition base*/
 		return (i);
 
 	return (-1);
