2767f021cccab7235d0d0d7b7b2451a79715ec04
Obumadunemejiamatu <obummadunaeme@gmail.com>
2023-01-06 17:31:49 -0800

    Task 0

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ * main - prins coins left
+ * @argc: parameter Entry
+ * @argv: one number of arguments
+ * Return: 0 one one number of arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	int c, coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	c = atoi(argv[1]);
+	if (c < 0)
+	{
+
+
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
+		{
