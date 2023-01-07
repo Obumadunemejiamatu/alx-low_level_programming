336ff760bb8e8f208498bfee75201cf047f97a14
Obumadunemejiamatu <obummadunaeme@gmail.com>
2023-01-07 02:35:25 -0800

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -34,15 +34,14 @@ int _atoi(char *s)
 			n = n * 10 + digit;
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
-				
 				break;
 			f = 0;
 		}
 		i++;
 	}
-	
 	if (f == 0)
 		return (0);
+
 	return (n);
 }
 
