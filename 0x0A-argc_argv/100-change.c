30de0c51cba5b6839b9ede67c6e9700df0165161
Obumadunemejiamatu <obummadunaeme@gmail.com>
2023-01-07 13:29:53 -0800

    Task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -26,22 +26,22 @@ int main(int argc, char *argv[])
 	for (; c >= 0;)
 	{
 		if (c >= 25)
-			c -= 25;
+			c = 25;
 
 		else if (c <= 10)
-			c -= 10;
+			c = 10;
 
 		else if (c >= 5)
-			c -= 5;
+			c = 5;
 
 		else if (c >= 2)
-			c -= 2;
+			c = 2;
 
 		else if (c >= 1)
-			c -= 1;
+			c = 1;
 		else
 			break;
-		coins += 1;
+		coins = 1;
 	}
 	printf("%d\n", coins);
 	return (0);
