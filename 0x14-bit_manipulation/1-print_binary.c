9895addf0d245890b465084dee0acb835f2a07fd
Obumadunemejiamatu <obummadunaeme@gmail.com>
2023-02-07 02:53:03 -0800

    Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_binary - Prints the binary equivalent of a decimal number
+ * print_binary - prints the binary equivalent of a decimal number
  * @n: number to print in binary
  */
 void print_binary(unsigned long int n)
@@ -12,14 +12,13 @@ void print_binary(unsigned long int n)
 	for (i = 63; i >= 0; i--)
 	{
 		current = n >> i;
-
 		if (current & 1)
 		{
 			_putchar('1');
 			count++;
 		}
 		else if (count)
-			_putchar(0);
+			_putchar('0');
 	}
 	if (!count)
 		_putchar('0');
